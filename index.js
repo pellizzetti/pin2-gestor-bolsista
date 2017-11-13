075c12854213734f2c4af7162a265bc5bc864636
Guilherme Pellizzetti <guipellizzetti@gmail.com>
2017-11-13 13:41:02 -0200

    App register

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import { AppRegistry } from 'react-native';
 import App from './src';
 
-AppRegistry.registerComponent('Gestor Bolsista', () => App);
+AppRegistry.registerComponent('gestorbolsista', () => App);
