754f2e9cf05dc40522beb11a1afd4fea0fa51322
Guilherme Pellizzetti <guipellizzetti@gmail.com>
2017-11-14 01:01:19 -0200

    Atualiza client http

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { HttpClientModule } from '@angular/common/http';
 import { IonicStorageModule } from '@ionic/storage';
 
 import { MyApp } from './app.component';
@@ -14,6 +15,8 @@ import { AuthServiceProvider } from '../providers/auth-service/auth-service';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    IonicModule.forRoot(MyApp),
     IonicStorageModule.forRoot()
   ],
   bootstrap: [IonicApp],
--- src/providers/auth-service/auth-service.ts
+++ src/providers/auth-service/auth-service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@angular/core";
 //import { Http } from '@angular/http';
 import { Observable } from "rxjs/Observable";
+import { HttpClient } from '@angular/common/http';
 import { Storage } from '@ionic/storage';
 import "rxjs/add/operator/map";
 
@@ -14,18 +15,37 @@ import { API_URL } from '../../app/constants';
 
 @Injectable()
 export class AuthServiceProvider {
-  currentUser: User;
+  constructor(private http: HttpClient, private storage: Storage) {}
 
   public login(credentials) {
     if (credentials.email === null || credentials.password === null) {
-      return Observable.throw("Preencha os campos para continuar");
+      return Observable.throw('Preencha os campos para continuar');
     } else {
       return Observable.create(observer => {
+
+        const body = {
+          email: credentials.email,
+          password: credentials.password
+        };
+
+        this.http
+          .post<Response>(`${API_URL}/login`, body)
+          .subscribe(
+            res => {
               this.storage.set('jwt', res.jwt)
                 .then(() => {
                   observer.next(res);
                   observer.complete();
                 });
+            },
+            err => {
+              if (err.error) {
+                observer.next(err.error);
+                observer.complete();
+              } else {
+                console.log('Ocorreu um erro', err);
+              }
+            }
         );
         observer.next(access);
         observer.complete();
