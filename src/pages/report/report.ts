8468f65ecf361052d7feb4fc3ac13fcc4d1272bd
Guilherme Pellizzetti <guipellizzetti@gmail.com>
2017-11-14 21:31:18 -0200

    Modulo/classe report

---
+++ src/pages/report/report.ts
@@ -0,0 +1,30 @@
+import { Component } from "@angular/core";
+import { NavController, IonicPage } from "ionic-angular";
+import { AuthServiceProvider } from "../../providers/auth-service/auth-service";
+
+@IonicPage()
+@Component({
+  selector: "page-report",
+  templateUrl: "report.html"
+})
+export class ReportPage {
+  username: string = "";
+  email: string = "";
+
+  constructor(private nav: NavController, private auth: AuthServiceProvider) {
+    this.auth.getUserInfo()
+      .then((userInfo) => {
+        const info = userInfo
+        console.log('info', info)
+      });
+
+    // this.username = info["name"];
+    // this.email = info["email"];
+  }
+
+  public logout() {
+    this.auth.logout().subscribe(succ => {
+      this.nav.setRoot("LoginPage");
+    });
+  }
+}
