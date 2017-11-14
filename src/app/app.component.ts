6cc56ce380de76107eff939b3cf12a52178d0d89
Guilherme Pellizzetti <guipellizzetti@gmail.com>
2017-11-14 18:35:55 -0200

    Navegação por menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,38 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { Platform, MenuController, Nav } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+
+import { HomePage } from '../pages/home/home';
 
 @Component({
   templateUrl: 'app.html'
 })
 
 export class MyApp {
+  @ViewChild(Nav) nav: Nav;
   rootPage: any = 'LoginPage';
+  pages: Array<{title: string, component: any}>
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(
+    public platform: Platform,
+    public menu: MenuController,
+    public statusBar: StatusBar,
+    public splashScreen: SplashScreen
+  ) {
+    this.pages = [
+      { title: 'Início', component: HomePage }
+    ];
+
     platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
     });
+  }
+
+  openPage(page) {
+    this.menu.close();
+    this.nav.setRoot(page.component);
   }
 }
 
