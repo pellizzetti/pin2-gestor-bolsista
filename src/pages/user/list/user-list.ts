706a2dea78af3d0faaf762c9f246eff862a9da95
Guilherme Pellizzetti <guipellizzetti@gmail.com>
2017-11-27 08:07:15 -0200

    Adiciona listagem de usuário

---
+++ src/pages/user/list/user-list.ts
@@ -0,0 +1,100 @@
+import { Component, OnInit } from "@angular/core";
+import { DatePipe } from "@angular/common"
+import {
+  NavController,
+  AlertController,
+  IonicPage,
+  Loading,
+  LoadingController
+} from "ionic-angular";
+
+import { AuthServiceProvider } from "../../../providers/auth-service/auth-service";
+import { UserServiceProvider } from "../../../providers/user-service/user-service";
+
+@IonicPage()
+@Component({
+  selector: "page-user-list",
+  templateUrl: "user-list.html"
+})
+export class UserListPage implements OnInit {
+  loadingData: Loading;
+  listUsers: any = [];
+  emptyList: boolean = false;
+
+  constructor(
+    private nav: NavController,
+    private authService: AuthServiceProvider,
+    private userService: UserServiceProvider,
+    private alertCtrl: AlertController,
+    private loadingCtrl: LoadingController
+  ) {}
+
+  async ngOnInit() {
+    const userDecoded = await this.authService.getUserInfo()
+    if (!userDecoded || userDecoded === null) {
+      this.nav.setRoot('LoginPage');
+    }
+    
+    if (userDecoded.userLevel === 'bolsista') {
+      this.nav.setRoot('HomePage');
+    }
+    
+    await this.getUserList();
+  }
+
+  private async getUserList() {
+    this.showLoading();
+
+    this.userService.getUserList().subscribe(
+        res => {
+          if (res.success) {
+            this.listUsers = res.usersList || [];
+
+            if (this.listUsers.length === 0) {
+              this.emptyList = true;
+            }
+
+            this.loadingData.dismiss();
+          } else if (res.message) {
+            this.showError(res.message);
+          } else {
+            this.showError('Não foi possível conectar com o servidor da API!');
+          }
+        },
+        err => {
+          this.showError(err);
+        }
+    );
+  }
+
+  private navigateToUser() {
+    this.nav.setRoot("UserPage");
+  }
+
+  public logout() {
+    this.authService.logout().subscribe(succ => {
+      this.nav.setRoot("LoginPage");
+    });
+  }
+
+  private showLoading() {
+    this.loadingData = this.loadingCtrl.create({
+      content: 'Buscando dados da API...',
+      dismissOnPageChange: true
+    });
+
+    this.loadingData.present();
+  }
+
+  private showError(text) {
+    this.loadingData.dismiss;
+
+    let alert = this.alertCtrl.create({
+      title: "Erro",
+      subTitle: text,
+      buttons: ["OK"]
+    });
+
+    alert.present(prompt);
+  }
+}
