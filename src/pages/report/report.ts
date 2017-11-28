62212acf97e40cb64b057c59e0230e96f0e2c675
Guilherme Pellizzetti <guipellizzetti@gmail.com>
2017-11-28 08:13:07 -0200

    Ajustes tela de relatório

--- src/pages/report/report.ts
+++ src/pages/report/report.ts
@@ -8,22 +8,33 @@ import { AuthServiceProvider } from "../../providers/auth-service/auth-service";
   templateUrl: "report.html"
 })
 export class ReportPage {
-  username: string = "";
-  email: string = "";
+  reports: Array<{title: string, component: any}>
 
-  constructor(private nav: NavController, private auth: AuthServiceProvider) {
-    this.auth.getUserInfo()
-      .then((userInfo) => {
-        const info = userInfo
-        console.log('info', info)
-      });
+  constructor(
+    private nav: NavController,
+    private authService: AuthServiceProvider
+   ) {
+    this.reports = [
+      { title: 'Usuários', component: 'HomePage' },
+      { title: 'Relatórios de controle', component: 'AttendanceListPage' },
+      { title: 'Folha ponto', component: 'UserListPage' },
+      { title: 'Relatório semestral', component: 'ReportPage' }
+    ];
+   }      
 
-    // this.username = info["name"];
-    // this.email = info["email"];
+  async ngOnInit() {
+    const userDecoded = await this.authService.getUserInfo();
+    if (!userDecoded || userDecoded === null) {
+      this.nav.setRoot('LoginPage');
+    }
+
+    if (userDecoded.userLevel === 'bolsista') {
+      this.nav.setRoot('HomePage');
+    }
   }
 
   public logout() {
-    this.auth.logout().subscribe(succ => {
+    this.authService.logout().subscribe(succ => {
       this.nav.setRoot("LoginPage");
     });
   }
